47e88dff5c4195c7ff5e6822cd926b393b3f0031
Tinh La <tla005@ucr.edu>
2019-05-16 15:14:36 -0700

    Create main.c

---
+++ Project/Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project.c
+ *
+ * Created: 5/16/2019 3:13:49 PM
+ * Author : tinhl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
