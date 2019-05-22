0d249deb084b27a56e64c783dbd80a7fd3aaa0b4
Tinh La <tla005@ucr.edu>
2019-05-21 19:31:05 -0700

    Create Project

---
+++ LCD_Racer/LCD_Racer/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCD_Racer.c
+ *
+ * Created: 5/21/2019 7:30:45 PM
+ * Author : tinhl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
