e3c4ae8974ec26e506972da46c35168a976f5c2b
Tinh La <tla005@ucr.edu>
2019-05-21 19:33:58 -0700

    Update main.c

--- LCD_Racer/LCD_Racer/main.c
+++ LCD_Racer/LCD_Racer/main.c
@@ -1,5 +1,6 @@
 /*
- * LCD_Racer.c
+ * LCD_Racer - CS120B
+ * UCR Spring 2019
  *
  * Created: 5/21/2019 7:30:45 PM
  * Author : tinhl
@@ -14,7 +15,10 @@
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRA = 0x00; PORTA = 0xFF; 
+	DDRB = 0xFF; PORTB = 0x40;
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00; 
     while (1) 
     {
     }
