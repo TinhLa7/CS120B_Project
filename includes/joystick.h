ce78cba8734da243b33b3317faf9a645ed80db5a
Tinh La <tla005@ucr.edu>
2019-06-05 10:10:13 -0700

    update

--- includes/joystick.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "defines.h"
-
-unssh coords[2];
-
-void ADC_init();
-void wait(long numOP);
-void updateJoyStick();
--- includes/not_player.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "defines.h"
-
-typedef struct not_player {
-	unsch drawPosition;
-	unsch image;
-	unsch type;
-} not_player;
